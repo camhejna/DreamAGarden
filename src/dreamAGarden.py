c8dbabf018c1238daf5b9cf6ff39c1057f16e3f2
camhejna <32603083+camhejna@users.noreply.github.com>
2020-03-13 16:33:21 -0400

    sketching out program flow with comments and simple methods

--- src/dreamAGarden.py
+++ src/dreamAGarden.py
@@ -1,11 +1,16 @@
 def runGarden():
     print('running the garden')
-
+    # initialize sensor vars
     # for each item...
     # check them,
     # and add to log file
     # decide if watering is required
+    if needsWater:
+        print('watering the garden')
     # send log to email/server/TBD
+
+def needsWater():
+    return True
 
 if __name__ == '__main__':
     print('I am main')
