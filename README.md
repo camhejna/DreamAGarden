05b123d9f2aa0c37dbf1c72d18a45a2b171e4caf
camhejna <32603083+camhejna@users.noreply.github.com>
2020-03-17 23:29:23 -0400

    created first API call to Adafruit IO

--- README.md
+++ README.md
@@ -7,5 +7,12 @@ another IOT Garden
 ## Dependencies ##
 
 [darkskylib](https://github.com/lukaskubis/darkskylib) - a python library for the [Dark Sky API](https://darksky.net/dev/docs).
+[Adafruit IO](https://github.com/adafruit/Adafruit_IO_Python) - a python library for the Adafruit IO API. All feeds are part of the default group, but are prefixed with 'dag_'. This is because, during early development, I continually received 404 errors when trying to access feeds in a different group. I did not have this issue when using feeds only in the default group.
+
+## I/O Components ##
+
+[Adafruit Soil Sensor](https://learn.adafruit.com/adafruit-stemma-soil-sensor-i2c-capacitive-moisture-sensor/overview)
+
+[AM2302 (wired DHT22) temperature-humidity sensor](https://learn.adafruit.com/dht)
 
 [version]: https://img.shields.io/badge/v-0.1-blue
--- src/dreamAGarden.py
+++ src/dreamAGarden.py
@@ -5,14 +5,36 @@ and decides whether or not to trigger the water pump"""
 # import y from temperatureHumiditySensor
 # import z from waterPump
 from datetime import datetime as dt
+from datetime import date
 from darksky import forecast
+from Adafruit_IO import Client, Feed, Data
 import keys
 
+#module vars
+aio = Client(keys.adausr, keys.adafruit)
+dag = 'dream-a-garden.'
+evl = keys.darksky, 42.2613, -78.6580
+today = date.today()
+
 def runGarden():
+    feeds = aio.feeds()
+    feedKeys = []
+    for f in feeds:
+        feedKeys.append(f.key)
+        #print('Feed: {0}, key: {1}'.format(f.name,f.key))
+    leKey = dag + 'last-execution'
+    if leKey in feedKeys:
+        lastExecution = Data(value=((dt.now()).isoformat()))
+        aio.create_data(leKey, lastExecution)
+        #TODO: log success
+    else:
+        #TODO: log error
+        print('could not find LE feed')
     print('running the garden')
     # initialize sensor vars
     # for each item...
     # check them,
+    
     # and add to log file
     # decide if watering is required
     if needsWater():
@@ -21,7 +43,6 @@ def runGarden():
 
 def needsWater():
     print('checking the weather')
-    evl = keys.darksky, 42.2613, -78.6580
     evlNow = forecast(*evl)
     print(evlNow['currently']['precipProbability'])
     retVal = False
@@ -44,9 +65,24 @@ def needsWater():
     #decide
     #return True
 
+def forecastFrost():
+    upcomingFrost = False
+    #next30Days = []
+    for i in range(30):
+        print(i)
+        print((dt(today.year, today.month, today.day+i, 0)).isoformat())
+        day = forecast(*evl, time=(dt(today.year, today.month, today.day+i, 0).isoformat()))
+        #next30Days.append(day['daily']['data'][0]['temperatureLow'])
+        print(day['daily']['data'][0]['temperatureLow'])
+        if(day['daily']['data'][0]['temperatureLow'] < 30):
+            upcomingFrost = True
+    return upcomingFrost
+
+
 def weatherCheck(darkskyLoc):
     print('hello')
 
 if __name__ == '__main__':
     print('I am main')
     runGarden()
+    #forecastFrost()
