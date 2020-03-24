17bb5ab77dfade0d9695a94ef595976fc973f3ef
camhejna <32603083+camhejna@users.noreply.github.com>
2020-03-24 09:54:21 -0400

    test class for dag_garden

---
+++ dreamAGarden/test_dag_garden.py
@@ -0,0 +1,11 @@
+import unittest
+from dag_garden import DAG_Garden
+
+class dag_gardenTest(unittest.TestCase):
+
+    goodWeatherCond = {}
+    badWeatherCond = {}
+    goodSoilCond = {}
+    badSoilCond = {}
+    goodFutureWeather = {}
+    badFutureWeather = {}
